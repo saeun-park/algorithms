c45f5804f301afb03bdff6a2b0675f11fa1916dd
Saeun Park <ajsklaoao@naver.com>
2026-01-22 19:35:48 +0900

    Time: 0 ms (100%), Space: 19.5 MB (12.08%) - LeetHub

--- LeetCode/1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ LeetCode/1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -5,8 +5,4 @@ class Solution:
         for a in arr:
             counts[a] = counts.get(a, 0) + 1
 
-        # len(counts) == len(set(counts.values()))
         return len(counts) == len(set(counts.values()))
-
-
-        
