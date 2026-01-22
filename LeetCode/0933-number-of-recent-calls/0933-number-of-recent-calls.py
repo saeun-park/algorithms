6639fd0ffdc8eea9a3647ef5a81cd81f1458b7f4
Saeun Park <ajsklaoao@naver.com>
2026-01-22 20:19:12 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ LeetCode/0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -2,15 +2,14 @@ from collections import deque
 class RecentCounter:
 
     def __init__(self):
-        # 필요한 변수 초기화 -- 상태를 저장할 자료구조 초기화
-        self.q = deque()    
+        self.q = deque()
 
     def ping(self, t: int) -> int:
         self.q.append(t)
-
-        while self.q[0] < t-3000:
+        if(self.q[0] < t-3000):
             self.q.popleft()
         return len(self.q)
+        
 
 
 # Your RecentCounter object will be instantiated and called as such:
