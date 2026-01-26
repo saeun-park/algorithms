63f79acb98015661de6c3cbb8700cafc3b1c2301
Saeun Park <ajsklaoao@naver.com>
2026-01-26 20:02:25 +0900

    Time: 0 ms (100%), Space: 19.5 MB (6.57%) - LeetHub

--- LeetCode/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ LeetCode/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,12 +1,9 @@
 class Solution:
+    memo = {0:0, 1:1, 2:1}
     def tribonacci(self, n: int) -> int:
 
-        if n==0: return 0
-        if n==1 or n==2: return 1
-
-        a, b, c = 0, 1, 1
-        for _ in range(3, n+1):
-            next = a + b + c
-            a, b, c = b, c, next
-
-        return next
+        if n in Solution.memo:
+            return Solution.memo[n]
+        
+        Solution.memo[n] = self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+        return Solution.memo[n]
