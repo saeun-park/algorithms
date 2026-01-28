4ef605bf40e30573db1ede48bfecddb63ee3ae48
Saeun Park <ajsklaoao@naver.com>
2026-01-28 16:46:15 +0900

    Time: 15 ms (17.59%), Space: 21.4 MB (8.78%) - LeetHub

--- LeetCode/0169-majority-element/0169-majority-element.py
+++ LeetCode/0169-majority-element/0169-majority-element.py
@@ -1,12 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         n = len(nums)
-        half_n = len(nums)/2
+        half_n = len(nums)//2
         d = {}
         for num in nums:
             d[num] = d.get(num,0)+1
 
             if d[num] > half_n:
                 return num
-
-        
