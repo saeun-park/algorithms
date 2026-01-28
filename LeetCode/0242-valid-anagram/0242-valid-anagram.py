ac3dde69739b63dde227bf71d0f6626f7adaff17
Saeun Park <ajsklaoao@naver.com>
2026-01-28 22:39:26 +0900

    Time: 6 ms (92.55%), Space: 19.6 MB (10.67%) - LeetHub

--- LeetCode/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,5 @@
+from collections import Counter 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        d1 = {}
-        d2 = {}
-        for x in s:
-            d1[x] = d1.get(x, 0)+1
-
-        for y in t:
-            d2[y] = d2.get(y, 0)+1
-
-        return d1==d2
+        return Counter(s) == Counter(t)
         
