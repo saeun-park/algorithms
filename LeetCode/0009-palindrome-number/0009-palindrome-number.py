2c28e5fc920c397ac7e90604312694e92700a881
Saeun Park <ajsklaoao@naver.com>
2026-01-28 15:53:34 +0900

    Time: 11 ms (37.37%), Space: 19.4 MB (21.56%) - LeetHub

--- LeetCode/0009-palindrome-number/0009-palindrome-number.py
+++ LeetCode/0009-palindrome-number/0009-palindrome-number.py
@@ -2,6 +2,6 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x<0:return False
 
-        str_x = str(abs(x))
+        str_x = str(x)
         reverse_x = str_x[::-1]
         return x==int(reverse_x)
