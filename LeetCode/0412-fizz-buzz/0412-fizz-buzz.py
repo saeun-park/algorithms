0f36079451ae2e56606f5fd99aaee3256e4852ce
Saeun Park <ajsklaoao@naver.com>
2026-01-28 20:40:22 +0900

    Time: 2 ms (27.73%), Space: 19.6 MB (21.89%) - LeetHub

---
+++ LeetCode/0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,17 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = [""]*n
+
+        for i in range(n):
+            if (i+1)%15==0:
+                res[i] = "FizzBuzz"
+
+            elif (i+1)%3==0:
+                res[i] = "Fizz"
+            elif (i+1)%5==0:
+                res[i] = "Buzz"
+            else:
+                res[i] = str(i+1)
+
+        return res
+        
