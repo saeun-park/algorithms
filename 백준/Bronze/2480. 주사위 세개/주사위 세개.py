92cf0f6afd3299618d136229798d96f29a2d183f
Saeun Park <ajsklaoao@naver.com>
2026-01-30 18:13:52 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,15 +1,11 @@
 li = list(map(int, input().split()))
-m = {}
+uniq = set(li)
 
-for i in li:
-    m[i] = m.get(i, 0)+1
-
-if len(m)==1:
+if len(uniq)==1:
     print(10000+li[0]*1000)
-elif len(m)==2:
-    for key, val in m.items():
-        if val==2:
-            print(1000+key*100)
+elif len(uniq)==2:
+    for x in uniq:
+        if li.count(x)==2:
+            print(1000+x*100)
 else:
     print(max(li)*100)
-    
