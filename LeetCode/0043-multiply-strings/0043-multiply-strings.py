5b6f861f92b230aed109c1287cbb30b9766b4226
Saeun Park <ajsklaoao@naver.com>
2026-01-30 15:36:35 +0900

    Time: 53 ms (25.06%), Space: 19.5 MB (13.76%) - LeetHub

---
+++ LeetCode/0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        l1 = len(num1)-1
+        l2 = len(num2)-1
+        res = [0]*(len(num1)+len(num2))
+
+        for i in range(l1, -1, -1):
+            for j in range(l2, -1, -1):
+                a = int(num1[i]) if i>=0 else 0
+                b = int(num2[j]) if j>=0 else 0
+                
+                s = (a*b)+int(res[i+j+1])
+                res[i+j+1] = s % 10
+                res[i+j] += s // 10
+        
+
+        res = "".join(map(str, res)).lstrip("0")
+        return res if res else "0"
+        
