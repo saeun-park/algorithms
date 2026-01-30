b345ceaab7f454e4c090ddf0eb500968ab296d82
Saeun Park <ajsklaoao@naver.com>
2026-01-30 18:25:52 +0900

    [Bronze III] Title: 오븐 시계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,12 +1,4 @@
 h, m = map(int, input().split())
 p = int(input())
 
-total_min = m+p
-
-if total_min<60:
-    print(h, total_min)
-else:
-    new_h = (h+total_min//60)%24
-    new_m = (total_min)%60
-    print(new_h, new_m)
-    
+print((h + (m+p)//60)%24, (m+p)%60)
