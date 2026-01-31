6cca5a70e453b8de9f9d46c664f49013e1fa0bc8
Saeun Park <ajsklaoao@naver.com>
2026-01-31 21:05:47 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 660 ms, Memory: 117036 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,9 @@
+from collections import Counter
+n = int(input())
+sangeun = list(map(int, input().split()))
+m = int(input())
+card = list(map(int, input().split()))
+
+count = Counter(sangeun)
+ans = [count[c] for c in card]
+print(*ans)
