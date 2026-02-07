9adb1a42b1a8e9a263333c3393f682c875aee334
Saeun Park <ajsklaoao@naver.com>
2026-02-07 21:12:06 +0900

    Time: 61 ms (67.2%), Space: 22.3 MB (14.28%) - LeetHub

--- LeetCode/Medium/1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ LeetCode/Medium/1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -6,11 +6,11 @@ class Solution:
         while right <= n-1:
             if nums[right] == 0:
                 zero_cnt += 1
-            right += 1
             
             while zero_cnt > k:
                 if nums[left] == 0:
                     zero_cnt -= 1
                 left += 1
-            max_len = max(max_len, right-left)
+            max_len = max(max_len, right-left+1)
+            right += 1
         return max_len
