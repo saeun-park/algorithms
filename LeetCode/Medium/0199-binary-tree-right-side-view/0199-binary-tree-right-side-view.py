57300496578698c0aa652c017b2d4a0dccd925c6
Saeun Park <ajsklaoao@naver.com>
2026-03-03 20:20:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -1,25 +1,21 @@
 from collections import deque
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        
         res = []
-        q = deque([root])
-        # while 한 번 = 한 레벨 처리
-        while q:
-            level_size = len(q)
-            # 해당 레벨의 노드 수만큼 반복
-            for i in range(level_size):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-                
-                if i==level_size-1:
-                    res.append(node.val)
+
+        def dfs(node, level):
+            if not node:
+                return
+            if level == len(res):
+                res.append(node.val)
+            
+            dfs(node.right, level+1)
+            dfs(node.left, level+1)
+
+        dfs(root, 0)
         return res
+        
+        
 
 
         
